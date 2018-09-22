f4e3822d851775172553d96ae1ec607f9871330e
Sarah Jane <godwolf107@gmail.com>
2018-09-22 20:23:36 +0700

    added some backend apis

--- backend/dexplan/urls.py
+++ backend/dexplan/urls.py
@@ -5,4 +5,11 @@ from . import seeds
 urlpatterns = [
     url(r'^hello', views.hello),
     url(r'^seed', seeds.seed),
+    url(r'^users', views.users),
+    url(r'^projects', views.projects),
+    url(r'^documents', views.documents),
+    url(r'^sheets', views.sheets),
+    url(r'^styles', views.styles),
+    url(r'^tasks', views.tasks),
+    url(r'^members', views.members),
 ]
--- backend/dexplan/views.py
+++ backend/dexplan/views.py
@@ -1,8 +1,61 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User, Group
+from .models import Project
+from .models import Document
+from .models import Sheet
+from .models import Style
+from .models import Task
+from .models import Member
+from .models import Setting
 
 # Create your views here.
 
 
 def hello(request):
     return HttpResponse("Hello world!")
+
+
+def users(request):
+    users = User.objects.all()
+
+    return JsonResponse(list(users.values()), safe=False)
+
+
+def projects(request):
+    user_id = request.GET.get('id')
+    projects = Project.objects.filter(user_id=user_id)
+
+    return JsonResponse(list(projects.values()), safe=False)
+
+def documents(request):
+    project_id = request.GET.get('id')
+    documents = Document.objects.filter(project_id=project_id)
+
+    return JsonResponse(list(documents.values()), safe=False)
+
+
+def sheets(request):
+    document_id = request.GET.get('id')
+    sheets = Sheet.objects.filter(document_id=document_id)
+
+    return JsonResponse(list(sheets.values()), safe=False)
+
+
+def styles(request):
+    sheet_id = request.GET.get('id')
+    styles = Style.objects.filter(sheet_id=sheet_id)
+
+    return JsonResponse(list(styles.values()), safe=False)
+
+def tasks(request):
+    sheet_id = request.GET.get('id')
+    tasks = Task.objects.filter(sheet_id=sheet_id)
+
+    return JsonResponse(list(tasks.values()), safe=False)
+
+def members(request):
+    project_id = request.GET.get('id')
+    members = Member.objects.filter(project_id=project_id)
+
+    return JsonResponse(list(members.values()), safe=False)
